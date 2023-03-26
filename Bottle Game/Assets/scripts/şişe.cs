d91717836c93681b7f8e380d9fc75dd5a5fcaf9f
emirselcukJr <emirselcukjr@gmail.com>
2023-03-27 01:51:31 +0300

    can ve yeniden başlatma

--- Bottle Game/Assets/scripts/şişe.cs
+++ Bottle Game/Assets/scripts/şişe.cs
@@ -1,11 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class şişe : MonoBehaviour
 {
     public Transform sise;
+    public TextMeshProUGUI canYazisi;
+    int can = 3;
 
+
+    private void Update()
+    {
+        canYazisi.text = "Can : " + can.ToString();
+
+        if (can== 0 )
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
 
     void OnCollisionEnter(Collision temas)
     {
@@ -14,6 +28,7 @@ public class şişe : MonoBehaviour
         if (temas.gameObject.tag == "zemin")
         {
             sise.position = new Vector3(rastgele, 6f, 0f);
+            can--;
         }
     }
 }
