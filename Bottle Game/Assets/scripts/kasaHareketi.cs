aaa014a972304cb0491b82d3e9228db2d83adbe4
emirselcukJr <emirselcukjr@gmail.com>
2023-03-27 01:42:09 +0300

    Puan sistemi eklendi

--- Bottle Game/Assets/scripts/kasaHareketi.cs
+++ Bottle Game/Assets/scripts/kasaHareketi.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class kasaHareketi : MonoBehaviour
 {
     public float hiz;
     public Transform sise;
+    int puan;
+    public TextMeshProUGUI puanYazisi;
 
 
     void Update()
     {
+
+        puanYazisi.text = "puan : " + puan.ToString();
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Translate(-hiz * Time.deltaTime, 0f, 0f);
@@ -29,6 +35,8 @@ public class kasaHareketi : MonoBehaviour
         if (Temas.gameObject.tag == "sise")
         {
             sise.position = new Vector3 (rastgele, 6f, 0f);
+
+            puan += 5;
         }
         
     }
